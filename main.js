5f111f4fc3dbf61bcf459b2251f10e7582a537e4
Alison Fay <alisonfay@Nicholass-MacBook-Pro.local>
2022-01-27 00:32:48 -0500

    name and date feature

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //enter Your name 
+  const nameBtn = document.getElementById('myNameBtn')
+  const name = document.getElementById('nameInput')
+  const nameP = document.getElementById('name')
 
+  nameBtn.addEventListener('click', () => {
+    nameP.innerText = `${name.value},`;
+    name,value = '';
+  })
+  
+
+  //generate a new Quote
   const quote = document.getElementById('quote');
   const button = document.getElementById('btn');
 
@@ -11,8 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((quote) => quote.json())
       .then((quote) => {
           const newQuote = document.getElementById("quote");
-          let i = getRandom(1642)
-          console.log(i)
+          let i = getRandom(1642);
           newQuote.innerText = `${quote[i]["text"]}`;
         //div.appendChild(newQuote);
       });
@@ -23,6 +33,12 @@ document.addEventListener("DOMContentLoaded", () => {
   function getRandom(max) {
     return Math.floor(Math.random()*max)
   }
-
+  
+  const today = new Date().toDateString();
+  const date = document.createElement('div');
+  date.setAttribute('id', 'date');
+  date.innerText = `${today}`;
+  document.body.appendChild(date);
+  
 });
 
